050a11df8d4dcde8a932f9b1fdb422b0c39e18b6
bjhoohaha <cvwo271828182845045>
2019-06-24 12:33:04 +0800

    rectified date issues with time_remaining

--- mydolist/app/controllers/tasks_controller.rb
+++ mydolist/app/controllers/tasks_controller.rb
@@ -22,16 +22,14 @@ class TasksController < ApplicationController
   end
 
   def time_remaining
-      @active_task = Task.where("status != 'Completed'")
-      @task_today = Task.where(:due_date => Date.today)
-      @task_week = @active_task.where("due_date <= ?", Date.today + 1.week)
-    # @task_today = Task.where("status != ? AND due_date == ?", "Completed", Date.today)
-    # @task_tomorrow = Task.where("status != ? AND due_date == ?", "Completed", Date.today + 1)
-    # @task_week = Task.where("status != ? AND due_date <= ? AND due_Date > ?", "Completed", Date.today + 1.week, Date.today + 1)
-    # @task_month = Task.where("status != ? AND due_date <= ? AND due_date > ? ", "Completed", Date.today + 1.month, Date.today + 1.week)
-    # @task_upcoming = Task.where("status != ? AND due_date > ?", "Completed", Date.today + 1.month)
-    # @task_overdue = Task.where("status != ? AND due_date < ?", "Completed", Date.today)
-    # @no_deadlines = Task.where("status != ? AND (due_date IS NULL)", "Completed")
+    @active_task = Task.where("status != 'Completed'")
+    @task_today = @active_task.where(:due_date => Date.today)
+    @task_tomorrow = @active_task.where(:due_date => (Date.today + 1))
+    @task_week = @active_task.where("due_date <= ? AND due_Date > ?", Date.today + 1.week, Date.today + 1)
+    @task_month = @active_task.where("due_date <= ? AND due_date > ? ", Date.today + 1.month, Date.today + 1.week)
+    @task_upcoming = @active_task.where("due_date > ?", Date.today + 1.month)
+    @task_overdue = @active_task.where("due_date < ?", Date.today)
+    @no_deadlines = @active_task.where("due_date IS NULL")
   end
 
 

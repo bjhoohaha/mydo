474ad9930d71301ced54ee520bf41c92b4805dd9
bjhoohaha <cvwo271828182845045>
2019-06-24 11:49:41 +0800

    Test date production

--- mydolist/app/controllers/tasks_controller.rb
+++ mydolist/app/controllers/tasks_controller.rb
@@ -22,7 +22,14 @@ class TasksController < ApplicationController
   end
 
   def time_remaining
-    @task_today = Task.where("status != ? AND due_date == ?", "Completed", Date.today)
+      @task_today = Task.where("status == ?", "Completed")
+      @task_tomorrow = Task.where("status == ?", "Completed")
+      @task_week = Task.where("status == ?", "Completed")
+      @task_month = Task.where("status == ?", "Completed")
+      @task_upcoming = Task.where("status == ?", "Completed")
+      @task_overdue = Task.where("status == ?", "Completed")
+      @no_deadlines = Task.where("status == ?", "Completed")
+    # @task_today = Task.where("status != ? AND due_date == ?", "Completed", Date.today)
     # @task_tomorrow = Task.where("status != ? AND due_date == ?", "Completed", Date.today + 1)
     # @task_week = Task.where("status != ? AND due_date <= ? AND due_Date > ?", "Completed", Date.today + 1.week, Date.today + 1)
     # @task_month = Task.where("status != ? AND due_date <= ? AND due_date > ? ", "Completed", Date.today + 1.month, Date.today + 1.week)
